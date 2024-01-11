5b2bbbced7f72a6db58391dfa55eddc21d466c5e
Shailendra Jain <49560419+sheltyj@users.noreply.github.com>
2024-01-11 09:13:16 +0530

    Time: 795 ms (29.92%), Space: 409.2 MB (5.07%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     
-    
+    vector<vector<int>> dp;
     
     bool fun(vector<int> &nums, int k, int mask, int sum, int currSum, int i){
         if(currSum==sum && k==1) return true;
         if(i==nums.size())return false;
         if(k==0) return false;
+        
+        if(dp[mask][k]!=-1) return dp[mask][k];
+        
         if(currSum==sum){
             //cout<<sum<<endl;
             //cout<<k<<endl;
@@ -14,7 +17,7 @@ public:
             //     if(vis[i])cout<<nums[i]<<" ";
             // }
             // cout<<endl;
-            return fun(nums,k-1,mask,sum,0,0);
+            return dp[mask][k] =  fun(nums,k-1,mask,sum,0,0);
         }
         bool taken = false;
         int currMask = (1<<i);
@@ -22,15 +25,17 @@ public:
             taken = fun(nums,k, (mask|currMask),sum,currSum+nums[i],i+1);
         }
         bool notTaken = fun(nums,k,mask,sum,currSum,i+1);
-        return taken || notTaken;
+        return dp[mask][k] = taken || notTaken;
     }
     
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        
+        int n = nums.size();
+        int mask = 1<<n;
+        dp.resize(mask+1,vector<int>(k+1,-1));
         int sum = 0;
         for(int i : nums) sum+=i;
         if(sum%k!=0) return false;
         vector<bool> vis(nums.size(),false);
-        return fun(nums,k,0,sum/k,0,0);   
+        return   fun(nums,k,0,sum/k,0,0);   
     }
 };
